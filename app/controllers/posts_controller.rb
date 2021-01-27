2f4aa3a2d609a02fbd017a967f2ed52c4d240e75
fukui-jin <jinfukui0130@icloud.com>
2021-01-27 18:53:37 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,9 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
+
   def create
-    Post.create(content: params[:content],checked: true)
+    post = Post.create(content: params[:content],checked: true)
     render json:{ post: post }
   end
   def checked
